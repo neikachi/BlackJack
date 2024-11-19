cdf082db23014cf89a08d1a4ed4338849a9419b4
neikachi <66822300+neikachi@users.noreply.github.com>
2024-11-18 20:11:10 -0800

    Files for users, players, dealers, tables, and a player driver.
    I updated the deck management folder to match the user, player, dealer, and table files.

---
+++ BlackJack/src/network/ConnectionHandler.java
@@ -0,0 +1,29 @@
+package network;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class ConnectionHandler implements Runnable{
+	private Socket connection;
+	
+	public ConnectionHandler(Socket currConnection) {
+		this.connection = currConnection;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			
+//			working on the individual connection
+			
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		} finally {
+			try {
+				connection.close();
+			} catch (IOException e) {
+				e.getStackTrace();
+			}
+		}
+	}
+}
