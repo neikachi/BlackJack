ea0e4594733552e5d8c361936ad8446381f9ea1c
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2024-11-28 16:15:36 -0800

    created Message class

--- src/network/Message.java
+++ src/network/Message.java
@@ -1,5 +1,50 @@
 package network;
 
-public class Message {
+import java.io.Serializable;
 
+public class Message implements Serializable{
+	private static int count = 0;
+	private final int id;
+	protected String type;
+	protected String status;
+	protected String text;
+	
+	public Message() {
+		this.type = "Undefined";
+		this.status = "Undefined";
+		this.text = "Undefined";
+		this.id = count ++;
+	}
+	
+	public Message(String type, String status, String text) {
+		this.type = type;
+		this.status = status;
+		this.text = text;
+		this.id = count ++;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public String getType() {
+		return this.type;
+	}
+	
+	public String getStatus() {
+		return this.status;
+	}
+	
+	public String getText() {
+		return this.text;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+	
 }
