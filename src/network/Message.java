93bf0408a604b160631e30e085df2b58106765e1
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2024-12-01 12:39:49 -0800

    updated Message class

--- src/network/Message.java
+++ src/network/Message.java
@@ -7,18 +7,24 @@ public class Message implements Serializable{
 	private final int id;
 	protected String type;
 	protected String role;
+	protected String username;
+	protected String password;
 	protected String text;
 	
 	public Message() {
 		this.type = "Undefined";
 		this.role = "Undefined";
+		this.username = "Undefined";
+		this.password = "Undefined";
 		this.text = "Undefined";
 		this.id = count ++;
 	}
 	
-	public Message(String type, String role, String text) {
+	public Message(String type, String role, String username, String password, String text) {
 		this.type = type;
 		this.role = role;
+		this.username = username;
+		this.password = password;
 		this.text = text;
 		this.id = count ++;
 	}
@@ -37,6 +43,22 @@ public class Message implements Serializable{
 	
 	public String getText() {
 		return this.text;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public void setPassword() {
+		this.password = password;
 	}
 	
 	public void setText(String text) {
