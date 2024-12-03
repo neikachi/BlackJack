bec2bf65c8f0f73d3564c74113b363c6fa28385a
Nani9k <victoranguiano28@gmail.com>
2024-12-03 02:10:57 -0800

    Modified deckManagement package for compatability purposes

--- src/deckManagement/Card.java
+++ src/deckManagement/Card.java
@@ -2,8 +2,26 @@ package deckManagement;
 
 public class Card {
     private final Suit suit;
-    private Rank rank;
-	public enum Suit { HEARTS, DIAMONDS, CLUBS, SPADES }
+    private final Rank rank;
+
+    public enum Suit { HEARTS, DIAMONDS, CLUBS, SPADES }
+
+    public enum Rank {
+        TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7),
+        EIGHT(8), NINE(9), TEN(10),
+        JACK(10), QUEEN(10), KING(10),
+        ACE(1);  // Default ACE value is 1
+
+        private final int value;
+
+        Rank(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
 
     public Card(Suit suit, Rank rank) {
         this.suit = suit;
@@ -21,9 +39,4 @@ public class Card {
     public int getValue() {
         return rank.getValue();
     }
-    
-    public void setAce(Rank rank) {
-    	this.rank = rank;
-    }
-
 }
